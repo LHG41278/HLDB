c9eaf6c4231e3d6216acaace6fb9c0088920255e
LHG <2020334843@qq.com>
2023-09-20 14:03:10 +0800

    complete some skiplist

--- src/include/skiplist.h
+++ src/include/skiplist.h
@@ -1,44 +1,21 @@
 #include <atomic>
+#include <exception>
 #include <optional>
 #include <vector>
+#include <random>
+#include "memory_pool.h"
 #include "status.h"
 
 namespace hldb {
     #define MAX_HEIGHT 12
-
-template <typename KeyType>
-struct Node {
-    const KeyType key;
-    std::atomic<Node *> next{};
-
-    explicit Node (const KeyType &OtherKey) : key{OtherKey} {}
-    Node (KeyType &&OtherKey) : key{std::move(OtherKey)} {}
-
-    void setNext(Node *next);
-
-    Node *Next();
-
-};
-
-
-template <typename KeyType>
-void Node<KeyType>::setNext(Node *next_ptr) {
-    next.store(next_ptr, std::memory_order_release);
-}
-
-template <typename KeyType>
-Node<KeyType>* Node<KeyType>::Next() {
-    next.load(std::memory_order_acquire);
-}
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
 class SkipList {
 public:
     SkipList() = delete;
 
-    explicit SkipList(const KeyComparator &comp) {
+    explicit SkipList(const KeyComparator &comp) : re{rd()}, uni_dist(1,MAX_HEIGHT) {                
         comparator = comp;
-        skipHead = buildNode();
     }
 
     status_t Insert(const KeyType &key, const ValueType &value);
@@ -48,37 +25,107 @@ public:
     status_t Delete(const KeyType &key);
 
 private:
-    struct SkipListNode {
+    struct Node {
         bool active;
+        const KeyType key;
         const ValueType value;
-        SkipListNode *next;
-        Node<KeyType> node[1];
+        const uint16_t height;
+        std::atomic<Node *> next[1];
+
+        bool setNext(int ext_height, Node *next) {
+            if (ext_height > height) {
+                return false;
+            }
+
+            next[ext_height - 1].store(next, std::memory_order_release);
+            return true;
+        }
+
+        std::optional<Node *> Next(int ext_height) {
+            if (ext_height > height) {
+                return std::nullopt;
+            }
+
+            return next[ext_height - 1].load(std::memory_order_acquire);
+        }
     };
-    std::atomic<int32_t> max_height{};
+
+    Node *head[MAX_HEIGHT];
+    std::atomic<uint32_t> max_height{};
     KeyComparator comparator;
-    SkipListNode* skipHead;
+    std::random_device rd;
+    std::default_random_engine re;
+    std::uniform_int_distribution<int> uni_dist;
+    MemoryPool memory_pool;
 
-    SkipListNode* buildNode();
+    uint32_t randomHeight();    
 
-    void releaseNode(SkipListNode *ptr);
+    Node *buildNode(uint64_t ran_hei) {
+        auto node =  static_cast<Node *>(memory_pool.AlignMemory(sizeof(bool) + 
+                sizeof(KeyType) + sizeof(ValueType) + 
+                sizeof(uint64_t) + sizeof(std::atomic<Node *>) * ran_hei));
+        node->height = ran_hei;
+        return node;
+    }
+ 
+    void releaseNode(Node *ptr) {
+        memory_pool.FreeMemory(ptr);
+    }
 
-    // return the target skip list node and the height of the result
-    std::pair<SkipListNode *, uint64_t> findLastLessOrEqualThan(const KeyType & key);
+    Node *findLastLessOrEqualThan(Node *start ,const KeyType &key, uint32_t height) {
+        if (start == nullptr) {
+            return nullptr;
+        }
+        Node *result = start;
+
+        while (result->Next(height) != nullptr && comparator(key, result->key) < 0) {
+            result = result->Next(height);
+        }
+
+        return result;
+    }    
 };
+
+
+template <typename KeyType, typename ValueType, typename KeyComparator>
+uint32_t SkipList<KeyType, ValueType, KeyComparator>::randomHeight() {
+    return uni_dist(re);
+}
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
 status_t SkipList<KeyType, ValueType, KeyComparator>::Insert(const KeyType &key, const ValueType &value) {
-    struct SkipPrev {
-        SkipListNode * levelNode{};
-        uint64_t height{};
-    };
+    Node *prev[MAX_HEIGHT];
+    uint32_t random_height = randomHeight();
 
-    SkipPrev prev[MAX_HEIGHT];
-    
+    max_height.load(std::memory_order_relaxed);
+    if (random_height > max_height) {
+        max_height.store(random_height, std::memory_order_relaxed);
+    }
+
+    for (int i = random_height - 1; i >= 0; i--) {
+        if (i != random_height - 1) [[likely]] {
+            prev[i] = prev[i + 1];
+        } else {
+            prev[i] = head;
+        }
+
+        prev[i] = findLastLessOrEqualThan(prev[i], key, i);
+    }
+
+    Node *newNode = buildNode(random_height);
+    for (int i = 0; i < random_height; i++) {
+        bool success = newNode->setNext(i, prev[i]->Next(i));
+        if (success) {
+            return status_t::MemoryAllocationError;
+        }
+        success = prev[i]->setNext(i, newNode);
+        if (success) {
+            return status_t::MemoryAllocationError;
+        }
+    }
+
+    return status_t::Normal;
 }
 }
-
-
-
 
 // template <typename KeyType, typename ValueType, typename KeyComparator>
